a0b662d797e4504a9b7664e4312296b481fdf25e
unknown <svetoslavbaykov55@gmail.com>
2025-01-15 10:03:48 +0200

    Commit

---
+++ table.py
@@ -0,0 +1,11 @@
+import sqlite3
+import pandas as pd
+
+
+conn = sqlite3.connect('supply_chain.db')
+
+df = pd.read_csv('supply_chain_data.csv')
+
+df.to_sql('Orders', conn, if_exists='replace', index=False)
+
+print("Data imported into SQLite successfully!")
